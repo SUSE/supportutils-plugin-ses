ef6b3dcf19e503580c6dd872d644093135e4b637
Tim Serong <tserong@suse.com>
2019-04-30 15:14:12 +1000

    update-rpm-list.sh: handle '~' in rpm versions

--- update-rpm-list.sh
+++ update-rpm-list.sh
@@ -9,6 +9,6 @@ SES_MEDIA_DIR=${1:-/srv/nfs/media/SUSE-Enterprise-Storage/}
 [[ ! -d $SES_MEDIA_DIR ]] && echo "Directory doesn't exist: $SES_MEDIA_DIR" && exit 1
 
 find $SES_MEDIA_DIR -name *.rpm | \
-	sed 's/.*\///; s/-[0-9.+A-Za-z_]\+-[0-9.]\+\.\(x86_64\|noarch\)\.rpm$//' | \
+	sed 's/.*\///; s/-[0-9.~+A-Za-z_]\+-[0-9.]\+\.\(x86_64\|noarch\)\.rpm$//' | \
 	grep -v '\.src\.rpm$' | \
 	sort > ses-rpm-list
